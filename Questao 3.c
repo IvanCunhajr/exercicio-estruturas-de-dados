0b81f0554b30c95f75bd56a3b09b50fd9333fa4f
IvanCunhajr <ivancunha198@gmail.com>
2026-03-19 09:27:57 -0300

    Create Questao 3.c

---
+++ Questao 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   int x, y, soma;
+   printf("Digite o primeiro numero:");
+   scanf("%d", &x);
+   printf("Digite o segundo numero:");
+   scanf("%d", &y);
+   
+    system("clear");
+   
+    soma=x+y;
+    printf("A soma eh:%d", soma);
+    return 0;
+}
