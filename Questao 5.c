10cc7d7b930c8090727eb68cd7a256b1a21fc70e
IvanCunhajr <ivancunha198@gmail.com>
2026-03-19 09:36:01 -0300

    Create Questao 5.c

---
+++ Questao 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   int x;
+   
+   printf("Digite um numero:");
+   scanf("%d", &x);
+   system("clear");
+   printf("Seu sucessor eh:%d", x+1 );
+   
+    return 0;
+}
